0552e3caa2deba1b4ecf64a25d716baf2c8f0046
Matthew Feng <matthew.f.phs@gmail.com>
2017-01-23 22:12:54 -0500

    added voltage estimator output

--- src/org/usfirst/frc/team4099/robot/loops/DashboardUpdater.java
+++ src/org/usfirst/frc/team4099/robot/loops/DashboardUpdater.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team4099.robot.loops;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4099.robot.subsystems.Drive;
 
 public class DashboardUpdater implements Loop {
 
     private Drive mDrive = Drive.getInstance();
+    private VoltageEstimator mVoltage = VoltageEstimator.getInstance();
     private static DashboardUpdater sInstance = new DashboardUpdater();
 
     public static DashboardUpdater getInstance() {
@@ -18,6 +20,7 @@ public class DashboardUpdater implements Loop {
 
     public synchronized void onLoop() {
         mDrive.outputToSmartDashboard();
+        SmartDashboard.putNumber("Average Voltage:", mVoltage.getAverageVoltage());
     }
 
 }
